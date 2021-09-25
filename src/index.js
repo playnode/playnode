cfbfaff5586f17a708850408cf657c4176617a94
Steven Robertson <steverobertson@gmail.com>
2021-09-25 15:11:17 +0100

    Converted to Create React App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { App } from "@playnode/react-music-components";
+import reportWebVitals from './reportWebVitals';
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'playnode.json', true);
 xhr.onload = () => {
     if (xhr.status === 200) {
         ReactDOM.render(
-            <App data={JSON.parse(xhr.responseText)}/>,
-            document.getElementById('root'));
+            // <React.StrictMode>
+                <App data={JSON.parse(xhr.responseText)}/>,
+            // </React.StrictMode>,
+            document.getElementById('root')
+        );
+
+        // If you want to start measuring performance in your app, pass a function
+        // to log results (for example: reportWebVitals(console.log))
+        // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+        reportWebVitals();
     }
 };
 
