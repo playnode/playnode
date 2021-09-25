cb94cfe24d5d4b6c0312db71892363eeb7e89f18
Steven Robertson <steverobertson@gmail.com>
2021-09-25 15:15:35 +0100

    Import JSON config into index.js via src

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from "@playnode/react-music-components";
+import { App } from '@playnode/react-music-components';
 import reportWebVitals from './reportWebVitals';
+import config from './playnode.json';
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'playnode.json', true);
-xhr.onload = () => {
-    if (xhr.status === 200) {
-        ReactDOM.render(
-            // <React.StrictMode>
-                <App data={JSON.parse(xhr.responseText)}/>,
-            // </React.StrictMode>,
-            document.getElementById('root')
-        );
+ReactDOM.render(
+    // <React.StrictMode>
+    <App data={config}/>,
+    // </React.StrictMode>,
+    document.getElementById('root')
+);
 
-        // If you want to start measuring performance in your app, pass a function
-        // to log results (for example: reportWebVitals(console.log))
-        // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-        reportWebVitals();
-    }
-};
-
-xhr.send();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
