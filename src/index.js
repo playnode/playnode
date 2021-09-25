a4793321854d409411db591464ae9f4b6812f146
Steven Robertson <steverobertson@gmail.com>
2021-09-25 02:24:36 +0100

    Imports App component from @playnode/music-website npm package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router } from "react-router-dom";
-import App from "./components/App";
+import { App } from "@playnode/music-website";
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'playnode.json', true);
