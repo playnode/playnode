bac0162597f715bdb860680056ee171fd5266469
Steven Robertson <steverobertson@gmail.com>
2021-09-25 10:12:29 +0100

    Moving router to App component in package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";
 import { App } from "@playnode/music-website";
 
 const xhr = new XMLHttpRequest();
@@ -8,9 +7,7 @@ xhr.open('GET', 'playnode.json', true);
 xhr.onload = () => {
     if (xhr.status === 200) {
         ReactDOM.render(
-            <Router>
-                <App data={JSON.parse(xhr.responseText)}/>
-            </Router>,
+            <App data={JSON.parse(xhr.responseText)}/>,
             document.getElementById('root'));
     }
 };
