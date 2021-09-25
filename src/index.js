f852247f461babefe928bffa14708ba363fcbb00
Steven Robertson <steverobertson@gmail.com>
2021-09-25 16:18:08 +0100

    Adding GitHub fork ribbon

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import { App } from '@playnode/react-music-components';
 import reportWebVitals from './reportWebVitals';
 import config from './playnode.json';
+import 'github-fork-ribbon-css/gh-fork-ribbon.css';
+import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
         <App data={config}/>
+        <a className="github-fork-ribbon right-bottom fixed"
+           href="https://github.com/playnode/react-music-website"
+           data-ribbon="Fork me on GitHub"
+           title="Fork me on GitHub">Fork me on GitHub</a>
     </React.StrictMode>,
     document.getElementById('root')
 );
